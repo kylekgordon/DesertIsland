be58058d1a6dad57067e9368052ecd2aab84ae2e
Mr.Gordon <58670552+kylekgordon@users.noreply.github.com>
2023-04-13 01:01:10 -0500

    Add files via upload

---
+++ beach-desert-2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="beach-desert-2" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="sprites/beach-desert-2.png" width="512" height="512"/>
+</tileset>
---
+++ sand.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="sand" tilewidth="32" tileheight="32" tilecount="108" columns="9">
+ <image source="sprites/sand.png" width="288" height="384"/>
+</tileset>
