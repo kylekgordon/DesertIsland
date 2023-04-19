01e41670ea83c13af14bff10a65b4118ef663391
Kyle Gordon <kylekgordon@gmail.com>
2023-04-19 14:41:06 -0500

    modified:   __pycache__/spritesheet.cpython-39.pyc 	modified:   main.py

--- main.py
+++ main.py
@@ -23,13 +23,13 @@ def main():
 
     Level1 = load_pygame('DesertIsland.tmx')
     Level1_group = pygame.sprite.Group()
-    player1 = pygame.image.load('sprites/pngegg.png').convert_alpha()
+    players = pygame.image.load('sprites/pngegg.png').convert_alpha()
     #print(tmx_data.layers) # Print all layers
 
     direction_x = 0
     direction_y = 0
 
-    sprite_sheet = spritesheet.SpriteSheet(player1)
+    player_1_sheet = spritesheet.SpriteSheet(players)
 
     # def get_player_image(player, frame, width, height, scale):
     #     player_image = pygame.Surface((width, height), pygame.SRCALPHA)
@@ -38,7 +38,7 @@ def main():
     #     return player_image
     
     player_animation = []
-    player_animation_steps = [3, 3, 3, 3]
+    player_animation_steps = 0
     action = 0
     player = 0
     last_update = pygame.time.get_ticks()
@@ -46,11 +46,12 @@ def main():
     player_frame = 0
     step_counter = 0
 
-    for x in range(3):
-        for y in range(4):
-            player_animation.append(sprite_sheet.get_player_image(x*32, y*32, 32, 32, 1))
+    # for x in range(3):
+    #     temp_list = []
+    #     for y in range(4):
+    #         player_animation.append(sprite_sheet.get_player_image(x*32, y*32, 32, 32, 1))
 
-    print(player_animation)
+    # print(player_animation)
     # for animation in player_animation_steps:
     #     temp_list = []
     #     for _ in range(animation):
@@ -59,11 +60,11 @@ def main():
     #     player_animation.append(temp_list)
     # #frame_0 = sprite_sheet.get_player_image(2, 32, 32, 1)
 
-    def blit_all_layers(window, tmx_data, world_offset):
+    def blit_all_layers(window, tmx_data, world_offset, start):
         for layer in tmx_data.visible_layers:
             if hasattr(layer, 'data'):
                 for image in layer.tiles():
-                    pos = (image[0] * 32 + world_offset[0], image[1] * 32 + world_offset[1])
+                    pos = (image[0] * 32 + world_offset[0]+ start[0], image[1] * 32 + world_offset[1]+ start[1])
                     window.blit(image[2], pos)
 
     # Get tiles for all layers
@@ -108,40 +109,27 @@ def main():
                     running = False
                 elif event.key == pygame.K_LEFT:
                     action = 1
-                    # if player_frame <= len(player_animation[action]):
-                    #     player_frame += 1
-                    # else:
-                    #     player_frame = 0
+                    player_animation_steps += 1
                     direction_x += 32
-                    # Level1_group.update(screen, (-32, 0))
+
                 elif event.key == pygame.K_RIGHT:
                     action = 2
-                    # if player_frame <= len(player_animation[action]):
-                    #     player_frame += 1
-                    # else:
-                    #     player_frame = 0
+                    player_animation_steps += 1
                     direction_x -= 32
-                    # Level1_group.update(screen, (32, 0))
+
                 elif event.key == pygame.K_UP:
                     action = 3
-                    # if player_frame <= len(player_animation[action]):
-                    #     player_frame += 1
-                    # else:
-                    #     player_frame = 0
+                    player_animation_steps += 1
                     direction_y += 32
-                    # Level1_group.update(screen, (0, -32))
+
                 elif event.key == pygame.K_DOWN:
                     action = 0
-                    # if player_frame <= len(player_animation[action]):
-                    #     player_frame += 1
-                    # else:
-                    #     player_frame = 0
+                    player_animation_steps += 1
                     direction_y -= 32
-                    # Level1_group.update(screen, (0, 32))
             
 
         screen.fill(black)    
-        blit_all_layers(screen, Level1, (direction_x, direction_y))
+        blit_all_layers(screen, Level1, (direction_x, direction_y), (-600,0))
 
         current_time = pygame.time.get_ticks()
 
@@ -153,11 +141,12 @@ def main():
         # if player_frame == len(player_animation[action]):
         #     player_frame = 0
 
-        screen.blit(player_animation[action], (400, 300))
+        screen.blit(player_1_sheet.get_player_image(player_animation_steps, action, 32, 32, 1), (400, 300))
 
-        # screen.blit(frame_0, (400, 300))
 
-        # Level1_group.draw(screen)
+        if player_animation_steps == 3:
+            player_animation_steps = 0
+        
         pygame.display.update()
         
         clock.tick(fps)
